4bc4ce5c22c8b56b6faeefb3f3b3d42e3490dcf5
Roy <royxzakanu@gmail.com>
2025-08-19 07:41:54 +0700

    Update kelola umkm dan berita admin

--- app/api/delete-berita/route.ts
+++ app/api/delete-berita/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-// Gunakan Service Role Key di server
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // pakai anon key aja
 );
 
 export async function POST(req: Request) {
@@ -15,7 +14,6 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: "ID tidak diberikan" }, { status: 400 });
     }
 
-    // Hapus dari Supabase
     const { error } = await supabase.from("berita").delete().eq("id", id);
 
     if (error) {
